ea43eceaf4f0edca90b4ecfcca60e6bd5b80c792
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-05-25 12:11:49 +0300

    Update card-template.js

--- src/templates/card-template.js
+++ src/templates/card-template.js
@@ -1,5 +1,30 @@
-export default cardTemplate = item => `
-  <div class='photo-card'>
+// export default cardTemplate = item => `<div class='photo-card'>
+//     <a href='${item.largeImageURL}'>
+//       <img src='${item.webformatURL}' alt='${item.tags}' loading='lazy' />
+//     </a>
+//     <div class='info'>
+//       <p class='info-item'>
+//         <b>Likes</b>
+//         ${item.likes}
+//       </p>
+//       <p class='info-item'>
+//         <b>Views</b>
+//         ${item.views}
+//       </p>
+//       <p class='info-item'>
+//         <b>Comments</b>
+//         ${item.comments}
+//       </p>
+//       <p class='info-item'>
+//         <b>Downloads</b>
+//         ${item.downloads}
+//       </p>
+//     </div>
+//   </div>
+// `;
+
+export default function cardTemplates(item) {
+  return `<div class='photo-card'>
     <a href='${item.largeImageURL}'>
       <img src='${item.webformatURL}' alt='${item.tags}' loading='lazy' />
     </a>
@@ -23,3 +48,4 @@ export default cardTemplate = item => `
     </div>
   </div>
 `;
+}
